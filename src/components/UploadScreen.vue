28b99c7d54f8286c19f8cc4e901fbf89cca160e8
Julien Blanchet <j55blanchet@gmail.com>
2021-04-06 14:02:33 -0400

    Add bulma & fileprovider

---
+++ src/components/UploadScreen.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section">
+
+  </section>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+});
+
+</script>
+
+<style>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 
+require('@/assets/main.scss');
+
 createApp(App).mount('#app');
