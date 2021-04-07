9f75fe5955c632e80ee4589604aec743e86dec58
Julien Blanchet <j55blanchet@gmail.com>
2021-04-06 22:52:38 -0400

    first publication

---
+++ docs/precache-manifest.b7554d6b2a9b4b94d6c24dd3cd49316a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62915fa0b9481687ddd",
+    "url": "/css/app.45f9e96c.css"
+  },
+  {
+    "revision": "e6f06a746a4797608cfb98a72c9acb70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c62915fa0b9481687ddd",
+    "url": "/js/app.bca8629b.js"
+  },
+  {
+    "revision": "f085f3503bf7eb7b0511",
+    "url": "/js/chunk-vendors.9c62212a.js"
+  },
+  {
+    "revision": "f1ed1df55ee059810c18e1f58d0193e1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
